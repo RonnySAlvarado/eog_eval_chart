abe9a1e1d7ae19416b6d315def166d525f3e6a3d
Ronny Alvarado <rsalvarado777@gmail.com>
2020-02-08 22:27:28 -0600

    Restructured data so that we're not doing a massive loop on every render, but on just initial render. Utilized useSubscription hook and moved the logic for shaping data from the Linegraph component to the useEffect hook that does it only once on initial data coming in

--- src/components/Linegraph.jsx
+++ src/components/Linegraph.jsx
@@ -2,23 +2,10 @@ import React from 'react';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer, Legend } from 'recharts';
 import moment from 'moment';
 
-const Linegraph = ({ data, view }) => {
-  const newDataArr = [];
-  for (let i = 0; i < 1382; i++) {
-    let newobj = {
-      at: data.getMultipleMeasurements[0].measurements[i].at,
-      waterTempVal: data.getMultipleMeasurements[0].measurements[i].value,
-      casingPressureVal: data.getMultipleMeasurements[1].measurements[i].value,
-      injValveOpenVal: data.getMultipleMeasurements[2].measurements[i].value,
-      flareTempVal: data.getMultipleMeasurements[3].measurements[i].value,
-      oilTempVal: data.getMultipleMeasurements[4].measurements[i].value,
-      tubingPressureVal: data.getMultipleMeasurements[5].measurements[i].value,
-    };
-    newDataArr.push(newobj);
-  }
+const Linegraph = ({ stateData, view }) => {
   return (
     <ResponsiveContainer height={500} width="80%">
-      <LineChart data={newDataArr}>
+      <LineChart data={stateData}>
         {view.includes('waterTemp') ? (
           <Line type="monotone" dataKey="waterTempVal" yAxisId="temperature" stroke="#6C345C" dot={false} />
         ) : null}
